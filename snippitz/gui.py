570c7e98333093a28555a41552c6391b54e93fca
Mikael-MStinson <67693304+Mikael-MStinson@users.noreply.github.com>
2021-07-14 13:52:22 -0500

    Created a blank canvas

---
+++ snippitz/gui.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+root.title('Node Mapper')
+canvas = Canvas(root, bg='white', width = 1600,height = 900)
+canvas.pack(expand = True, fill = BOTH)
+root.mainloop()
+
