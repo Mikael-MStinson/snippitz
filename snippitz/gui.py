d91f300f9bdd1e6f12be5415b276f9fbcf3597f9
Mikael-MStinson <67693304+Mikael-MStinson@users.noreply.github.com>
2021-07-14 14:42:38 -0500

    added a means of zooming in and out

--- snippitz/gui.py
+++ snippitz/gui.py
@@ -4,15 +4,40 @@ root = Tk()
 root.title('Node Mapper')
 canvas = Canvas(root, bg='white', width = 1600,height = 900)
 
+scale = 1.0
+
+class Circle:
+	def __init__(self, x, y, radius):
+		self.x = x
+		self.y = y
+		self.radius = radius
+		
+circles = []
+
+def update_canvas():
+	canvas.delete("all")
+	for circle in circles:
+		coord = (circle.x-circle.radius)*scale, (circle.y-circle.radius)*scale, (circle.x+circle.radius)*scale, (circle.y+circle.radius)*scale
+		canvas.create_oval(coord, outline="black", fill="white",width=2)
+		canvas.pack(expand = True, fill = BOTH)
+
 def place_circle(x,y, radius):
-	coord = x-radius, y-radius, x+radius, y+radius
-	canvas.create_oval(coord, outline="black", fill="white",width=2)
-	canvas.pack(expand = True, fill = BOTH)
+	circles.append(Circle(x,y, radius))
+	update_canvas()
 	
 def click_event(event):
 	place_circle(event.x, event.y, 100)
 
+def scroll_event(event):
+	global scale
+	if event.delta > 0:
+		scale += 0.01
+	elif event.delta < 0:
+		scale -= 0.01
+	update_canvas()
+
 canvas.pack(expand = True, fill = BOTH)
 root.bind("<Button>", click_event)
+root.bind("<MouseWheel>", scroll_event)
 root.mainloop()
 
