d12aee5fd097a586db9050a6613716c48ff2a29b
Mikael-MStinson <67693304+Mikael-MStinson@users.noreply.github.com>
2021-07-14 14:53:23 -0500

    Added a very basic pan mechanism
    there's a bug when zooming, new circles placed are at the mouses unscaled coordinates.
    There's also an issue with the zoom not moving in and out at the mouses location.
    And when panning, a new circle is created.

--- snippitz/gui.py
+++ snippitz/gui.py
@@ -1,3 +1,9 @@
+'''
+when zooming in and out, the mouse is not used as an origin to zoom into
+
+likewise, when placing a circle at a different scale, the mouse coordinates are not scaled
+'''
+
 from tkinter import *
 
 root = Tk()
@@ -5,6 +11,10 @@ root.title('Node Mapper')
 canvas = Canvas(root, bg='white', width = 1600,height = 900)
 
 scale = 1.0
+pan_x = 0
+pan_y = 0
+last_mouse_x = 0
+last_mouse_y = 0
 
 class Circle:
 	def __init__(self, x, y, radius):
@@ -17,16 +27,18 @@ circles = []
 def update_canvas():
 	canvas.delete("all")
 	for circle in circles:
-		coord = (circle.x-circle.radius)*scale, (circle.y-circle.radius)*scale, (circle.x+circle.radius)*scale, (circle.y+circle.radius)*scale
+		coord = (circle.x-circle.radius)*scale+pan_x, (circle.y-circle.radius)*scale+pan_y, (circle.x+circle.radius)*scale+pan_x, (circle.y+circle.radius)*scale+pan_y
 		canvas.create_oval(coord, outline="black", fill="white",width=2)
 		canvas.pack(expand = True, fill = BOTH)
 
-def place_circle(x,y, radius):
-	circles.append(Circle(x,y, radius))
-	update_canvas()
 	
 def click_event(event):
-	place_circle(event.x, event.y, 100)
+	global last_mouse_x
+	global last_mouse_y
+	last_mouse_x = event.x
+	last_mouse_y = event.y
+	circles.append(Circle(event.x, event.y, 100))
+	update_canvas()
 
 def scroll_event(event):
 	global scale
@@ -35,9 +47,21 @@ def scroll_event(event):
 	elif event.delta < 0:
 		scale -= 0.01
 	update_canvas()
+	
+def drag_event(event):
+	global pan_x
+	global pan_y
+	global last_mouse_x
+	global last_mouse_y
+	pan_x += event.x - last_mouse_x
+	pan_y += event.y - last_mouse_y
+	last_mouse_x = event.x
+	last_mouse_y = event.y
+	update_canvas()
 
 canvas.pack(expand = True, fill = BOTH)
 root.bind("<Button>", click_event)
 root.bind("<MouseWheel>", scroll_event)
+canvas.bind("<B1-Motion>", drag_event) 
 root.mainloop()
 
