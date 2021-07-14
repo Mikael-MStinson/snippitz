36e16e4821c78e011ee3577f4f755953484c496c
Mikael-MStinson <67693304+Mikael-MStinson@users.noreply.github.com>
2021-07-14 14:09:43 -0500

    added a way of placing circles on the canvas when the user clicks on the canvas

--- snippitz/gui.py
+++ snippitz/gui.py
@@ -7,8 +7,12 @@ canvas = Canvas(root, bg='white', width = 1600,height = 900)
 def place_circle(x,y, radius):
 	coord = x-radius, y-radius, x+radius, y+radius
 	canvas.create_oval(coord, outline="black", fill="white",width=2)
-place_circle(300,500,100)
+	canvas.pack(expand = True, fill = BOTH)
+	
+def click_event(event):
+	place_circle(event.x, event.y, 100)
 
 canvas.pack(expand = True, fill = BOTH)
+root.bind("<Button>", click_event)
 root.mainloop()
 
