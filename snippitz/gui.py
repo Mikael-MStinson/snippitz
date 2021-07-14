acf91d7eb9684eee41d0db996aa6a6f1d24b21f3
Mikael-MStinson <67693304+Mikael-MStinson@users.noreply.github.com>
2021-07-14 14:02:11 -0500

    created a means of placing a circle

--- snippitz/gui.py
+++ snippitz/gui.py
@@ -3,6 +3,12 @@ from tkinter import *
 root = Tk()
 root.title('Node Mapper')
 canvas = Canvas(root, bg='white', width = 1600,height = 900)
+
+def place_circle(x,y, radius):
+	coord = x-radius, y-radius, x+radius, y+radius
+	canvas.create_oval(coord, outline="black", fill="white",width=2)
+place_circle(300,500,100)
+
 canvas.pack(expand = True, fill = BOTH)
 root.mainloop()
 
